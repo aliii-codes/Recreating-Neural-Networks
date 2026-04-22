d05508b6c06a0c268e4033372e00a93fa343834f
aliii-codes <alikhan4566544@gmail.com>
2026-04-22 09:47:49 -0700

    feat: implemented our first ever neuron

---
+++ 02_basic_neurons/chapter_2_neurons.py
@@ -0,0 +1,10 @@
+inputs = [1,2,3]
+weights = [0.2,0.4,0.6]
+bias = 2
+
+output = (inputs[0]*weights[0]+
+          inputs[1]*weights[1]+
+          inputs[2]*weights[2]+
+          bias)
+
+print(output)
